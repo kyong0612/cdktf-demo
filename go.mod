21795f19c2d8f46fab67baa1f75f08f4563d5461
kimkiyong <fenderst0612@gmail.com>
2022-08-14 22:36:25 +0900

    feat: aws resource

--- go.mod
+++ go.mod
@@ -2,10 +2,11 @@ module cdk.tf/go/stack
 
 go 1.16
 
-require github.com/aws/constructs-go/constructs/v10 v10.1.56
+require github.com/aws/constructs-go/constructs/v10 v10.1.73
 
 require (
-	github.com/aws/jsii-runtime-go v1.62.0
+	github.com/aws/jsii-runtime-go v1.63.2
+	github.com/hashicorp/cdktf-provider-aws-go/aws/v9 v9.0.14 // indirect
 	github.com/hashicorp/cdktf-provider-docker-go/docker v1.0.2 // indirect
 	github.com/hashicorp/terraform-cdk-go/cdktf v0.12.0
 )
--- main.go
+++ main.go
@@ -3,27 +3,26 @@ package main
 import (
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
-	"github.com/hashicorp/cdktf-provider-docker-go/docker"
 
+	"github.com/hashicorp/cdktf-provider-aws-go/aws/v9"
+	"github.com/hashicorp/cdktf-provider-aws-go/aws/v9/ec2"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
 func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 	stack := cdktf.NewTerraformStack(scope, &id)
 
-	docker.NewDockerProvider(stack, jsii.String("docker"), &docker.DockerProviderConfig{})
-
-	dockerImage := docker.NewImage(stack, jsii.String("nginxImage"), &docker.ImageConfig{
-		Name:        jsii.String("nginx:latest"),
-		KeepLocally: jsii.Bool(false),
+	aws.NewAwsProvider(stack, jsii.String("AWS"), &aws.AwsProviderConfig{
+		Region: jsii.String("ap-northeast-1"),
 	})
 
-	docker.NewContainer(stack, jsii.String("nginxContainer"), &docker.ContainerConfig{
-		Image: dockerImage.Latest(),
-		Name:  jsii.String("tutorial"),
-		Ports: &[]*docker.ContainerPorts{{
-			Internal: jsii.Number(80), External: jsii.Number(8000),
-		}},
+	instance := ec2.NewInstance(stack, jsii.String("compute"), &ec2.InstanceConfig{
+		Ami:          jsii.String("ami-01456a894f71116f2"),
+		InstanceType: jsii.String("t2.micro"),
+	})
+
+	cdktf.NewTerraformOutput(stack, jsii.String("public_ip"), &cdktf.TerraformOutputConfig{
+		Value: instance.PublicIp(),
 	})
 
 	return stack
@@ -31,8 +30,12 @@ func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 
 func main() {
 	app := cdktf.NewApp(nil)
-
-	NewMyStack(app, "learn-cdktf-docker")
+	stack := NewMyStack(app, "aws_instance")
+	cdktf.NewRemoteBackend(stack, &cdktf.RemoteBackendProps{
+		Hostname:     jsii.String("app.terraform.io"),
+		Organization: jsii.String("<YOUR_ORG>"),
+		Workspaces:   cdktf.NewNamedRemoteWorkspace(jsii.String("learn-cdktf")),
+	})
 
 	app.Synth()
 }
